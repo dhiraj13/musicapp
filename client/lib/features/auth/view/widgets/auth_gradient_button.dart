77088a7011b909a1599f2f35c0729f1cc6a43d5f
Dhiru <dkhapangi13@outlook.com>
2024-09-23 17:38:42 +0545

    login page ui

--- client/lib/features/auth/view/widgets/auth_gradient_button.dart
+++ client/lib/features/auth/view/widgets/auth_gradient_button.dart
@@ -2,7 +2,10 @@ import 'package:client/core/theme/app_pallete.dart';
 import 'package:flutter/material.dart';
 
 class AuthGradientButton extends StatelessWidget {
-  const AuthGradientButton({super.key});
+  final String buttonText;
+  final VoidCallback onTap;
+  const AuthGradientButton(
+      {super.key, required this.buttonText, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
@@ -19,15 +22,15 @@ class AuthGradientButton extends StatelessWidget {
         borderRadius: BorderRadius.circular(7),
       ),
       child: ElevatedButton(
-        onPressed: () {},
+        onPressed: onTap,
         style: ElevatedButton.styleFrom(
           fixedSize: const Size(395, 55),
           backgroundColor: Pallete.transparentColor,
           shadowColor: Pallete.transparentColor,
         ),
         child: Text(
-          'Sign Up',
-          style: TextStyle(
+          buttonText,
+          style: const TextStyle(
             fontSize: 17,
             fontWeight: FontWeight.w600,
           ),
