65b9b708f41235645c705f1c957d9fab4aa366c9
dhiraj khapangi <dkhapangi13@outlook.com>
2025-01-29 20:54:30 +0545

    code refactoring signup api

--- server/main.py
+++ server/main.py
@@ -1,52 +1,12 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-from sqlalchemy import TEXT, VARCHAR, Column, LargeBinary, create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-import uuid
-import bcrypt
+from fastapi import FastAPI
+
+from models.base import Base
+from database import engine
+
+from routes import auth
 
 app = FastAPI()
 
-DATABASE_URL = 'postgresql://postgres:test123@localhost:5432/musicapp'
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-db = SessionLocal()
-
-class UserCreate(BaseModel):
-    name: str
-    email: str
-    password: str
-
-Base = declarative_base()
-
-class User(Base):
-    __tablename__ = 'users'
-
-    id = Column(TEXT, primary_key=True)
-    name = Column(VARCHAR(100))
-    email = Column(VARCHAR(100))
-    password = Column(LargeBinary)
-
-@app.post('/signup')
-def signup_user(user: UserCreate):
-    # check if the user already exists in db
-    user_db = db.query(User).filter(User.email == user.email).first()
-
-    if user_db:
-        raise HTTPException(409, 'User with the same email already exists!')
-    
-    hashed_pw = bcrypt.hashpw(user.password.encode(), bcrypt.gensalt(16))
-    user_db = User(id=str(uuid.uuid4()), email=user.email,
-                   password=hashed_pw, name=user.name)
-
-    # add the user to the db
-    db.add(user_db)
-    db.commit()
-    db.refresh(user_db)
-
-    return user_db
+app.include_router(auth.router, prefix='/auth')
 
 Base.metadata.create_all(engine)
---
+++ server/routes/auth.py
@@ -0,0 +1,29 @@
+import uuid
+import bcrypt
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from database import get_db
+from models.user import User
+from pydantic_schemas.user_create import UserCreate
+
+router = APIRouter()
+
+@router.post('/signup')
+def signup_user(user: UserCreate, db: Session=Depends(get_db)):
+    # check if the user already exists in db
+    user_db = db.query(User).filter(User.email == user.email).first()
+
+    if user_db:
+        raise HTTPException(409, 'User with the same email already exists!')
+
+    hashed_pw = bcrypt.hashpw(user.password.encode(), bcrypt.gensalt(16))
+    user_db = User(id=str(uuid.uuid4()), email=user.email,
+                   password=hashed_pw, name=user.name)
+
+    # add the user to the db
+    db.add(user_db)
+    db.commit()
+    db.refresh(user_db)
+
+    return user_db
