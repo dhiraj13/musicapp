262eaeb3f509469e13841b22ea92c052efb30457
dhiraj khapangi <dkhapangi13@outlook.com>
2025-01-29 20:24:10 +0545

    signup route, postgresql setup & sqlalchemy

--- server/main.py
+++ server/main.py
@@ -1,13 +1,52 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from sqlalchemy import TEXT, VARCHAR, Column, LargeBinary, create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import uuid
+import bcrypt
 
 app = FastAPI()
 
-class Test(BaseModel):
-    name: str
-    age: int
+DATABASE_URL = 'postgresql://postgres:test123@localhost:5432/musicapp'
 
-@app.post('/')
-def test(t: Test):
-    print(t)
-    return 'hello world'
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+db = SessionLocal()
+
+class UserCreate(BaseModel):
+    name: str
+    email: str
+    password: str
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(TEXT, primary_key=True)
+    name = Column(VARCHAR(100))
+    email = Column(VARCHAR(100))
+    password = Column(LargeBinary)
+
+@app.post('/signup')
+def signup_user(user: UserCreate):
+    # check if the user already exists in db
+    user_db = db.query(User).filter(User.email == user.email).first()
+
+    if user_db:
+        raise HTTPException(409, 'User with the same email already exists!')
+    
+    hashed_pw = bcrypt.hashpw(user.password.encode(), bcrypt.gensalt(16))
+    user_db = User(id=str(uuid.uuid4()), email=user.email,
+                   password=hashed_pw, name=user.name)
+
+    # add the user to the db
+    db.add(user_db)
+    db.commit()
+    db.refresh(user_db)
+
+    return user_db
+
+Base.metadata.create_all(engine)
