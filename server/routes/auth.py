43d842566e84ea6cbea373248e4ff389c31405dd
dhiraj khapangi <dkhapangi13@outlook.com>
2025-01-30 12:36:38 +0545

    login api route

--- server/routes/auth.py
+++ server/routes/auth.py
@@ -6,10 +6,11 @@ from sqlalchemy.orm import Session
 from database import get_db
 from models.user import User
 from pydantic_schemas.user_create import UserCreate
+from pydantic_schemas.user_login import UserLogin
 
 router = APIRouter()
 
-@router.post('/signup')
+@router.post('/signup', status_code=201)
 def signup_user(user: UserCreate, db: Session=Depends(get_db)):
     # check if the user already exists in db
     user_db = db.query(User).filter(User.email == user.email).first()
@@ -26,4 +27,20 @@ def signup_user(user: UserCreate, db: Session=Depends(get_db)):
     db.commit()
     db.refresh(user_db)
 
+    return user_db
+
+@router.post('/login')
+def login_user(user: UserLogin, db: Session=Depends(get_db)):
+    # check if a user with same email already exist
+    user_db = db.query(User).filter(User.email == user.email).first()
+
+    if not user_db:
+        raise HTTPException(404, 'User with this email does not exist!')
+    
+    # password matching or not
+    is_match = bcrypt.checkpw(user.password.encode(), user_db.password)
+    
+    if not is_match:
+        raise HTTPException(401, 'Incorrect password!')
+    
     return user_db
