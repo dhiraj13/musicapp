eaaae969417452ef2849a76acfd95cf7da2ac84b
dhiraj khapangi <dkhapangi13@outlook.com>
2025-01-31 15:08:40 +0545

    client side login and signup route integration

---
+++ client/lib/core/constants/server_constant.dart
@@ -0,0 +1,3 @@
+class ServerConstant {
+  static const String serverURL = 'https://9a8b-2400-1a00-b030-b34a-ed73-6105-a00-5794.ngrok-free.app';
+}
--- client/lib/features/auth/view/pages/signup_page.dart
+++ client/lib/features/auth/view/pages/signup_page.dart
@@ -1,7 +1,10 @@
 import 'package:client/core/theme/app_pallete.dart';
+import 'package:client/features/auth/repository/auth_remote_repository.dart';
+import 'package:client/features/auth/view/pages/login_page.dart';
 import 'package:client/features/auth/view/widgets/auth_gradient_button.dart';
 import 'package:client/features/auth/view/widgets/custom_field.dart';
 import 'package:flutter/material.dart';
+import 'package:fpdart/fpdart.dart' as fpdart;
 
 class SignupPage extends StatefulWidget {
   const SignupPage({super.key});
@@ -61,23 +64,44 @@ class _SignupPageState extends State<SignupPage> {
               const SizedBox(height: 20),
               AuthGradientButton(
                 buttonText: 'Sign up',
-                onTap: () {},
+                onTap: () async {
+                  final res = await AuthRemoteRepository().signup(
+                    name: nameController.text,
+                    email: emailController.text,
+                    password: passwordController.text,
+                  );
+
+                  final val = switch (res) {
+                    fpdart.Left(value: final l) => l,
+                    fpdart.Right(value: final r) => r.name,
+                  };
+                },
               ),
               const SizedBox(height: 20),
-              RichText(
-                  text: TextSpan(
-                text: 'Already have an account? ',
-                style: Theme.of(context).textTheme.titleMedium,
-                children: const [
-                  TextSpan(
-                    text: 'Sign In',
-                    style: TextStyle(
-                      color: Pallete.gradient2,
-                      fontWeight: FontWeight.bold,
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const LoginPage(),
                     ),
-                  ),
-                ],
-              ))
+                  );
+                },
+                child: RichText(
+                    text: TextSpan(
+                  text: 'Already have an account? ',
+                  style: Theme.of(context).textTheme.titleMedium,
+                  children: const [
+                    TextSpan(
+                      text: 'Sign In',
+                      style: TextStyle(
+                        color: Pallete.gradient2,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                )),
+              )
             ],
           ),
         ),
