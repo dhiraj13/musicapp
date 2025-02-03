e69e913a25e3cf9464b25034aeda9cfb3863ea94
dhiraj khapangi <dkhapangi13@outlook.com>
2025-02-03 19:12:29 +0545

    play background music

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -6,9 +6,15 @@ import 'package:client/features/home/view/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:just_audio_background/just_audio_background.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await JustAudioBackground.init(
+    androidNotificationChannelId: 'com.ryanheise.bg_demo.channel.audio',
+    androidNotificationChannelName: 'Audio playback',
+    androidNotificationOngoing: true,
+  );
   await Hive.initFlutter();
   await Hive.openBox('localSongs');
   final container = ProviderContainer();
